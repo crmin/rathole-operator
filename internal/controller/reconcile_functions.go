cbd5107b159d5b6cd448ec49c3bff748fe6b0fd7
crmin <crmin@conalog.com>
2024-05-16 19:02:09 +0900

    [WIP] Write ReconcileClient(); need test

--- internal/controller/reconcile_functions.go
+++ internal/controller/reconcile_functions.go
@@ -60,7 +60,7 @@ func ReconcileServer(r Reconciler, ctx context.Context, server *ratholev1alpha1.
 	}
 
 	// Set default token if set .Spec.DefaultTokenFrom and .Spec.DefaultToken is empty
-	// If both .Spec.DefaultTokenFrom and .Spec.DefaultToken are set, an error should occur through the webhook validate
+	// TODO: If both .Spec.DefaultTokenFrom and .Spec.DefaultToken are set, an error should occur through the webhook validate
 	if server.Spec.DefaultToken == "" {
 		if server.Spec.DefaultTokenFrom.ConfigMapRef.Name != "" { // Use ConfigMap
 			var (
@@ -94,6 +94,7 @@ func ReconcileServer(r Reconciler, ctx context.Context, server *ratholev1alpha1.
 	server.Status.Condition.ObservedGeneration = server.Generation
 	server.Status.Condition.Status = "Synced"
 	server.Status.Condition.Reason = "Reconciled"
+	// TODO: update last synced time
 	if err := r.Status().Update(ctx, server); err != nil {
 		return err
 	}
@@ -177,6 +178,183 @@ func ReconcileServer(r Reconciler, ctx context.Context, server *ratholev1alpha1.
 				ObjectMeta: metav1.ObjectMeta{
 					Name:            server.Spec.ConfigTarget.Name,
 					Namespace:       server.Namespace,
+					OwnerReferences: ownerRefs,
+				},
+				Data: map[string]string{
+					"config.toml": config,
+				},
+			}
+			if err := r.Create(ctx, &configMap); err != nil {
+				return err
+			}
+		} else {
+			configMap.Data["config.toml"] = config
+			if err := r.Update(ctx, &configMap); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func ReconcileClient(r Reconciler, ctx context.Context, client_ *ratholev1alpha1.RatholeClient) error {
+	if client_.Status.Condition.ObservedGeneration == client_.Generation { // Skip if already reconciled. Spec hasn't changed
+		return nil
+	}
+
+	// Get services linked to this server
+	var services ratholev1alpha1.RatholeServiceList
+	if err := r.List(ctx, &services, client.InNamespace(client_.Namespace)); err != nil {
+		return err
+	}
+
+	client_.Spec.Services = make(map[string]*ratholev1alpha1.RatholeServiceSpec)
+
+	for _, service := range services.Items {
+		for _, clientRef := range service.Spec.ClientRefs {
+			if clientRef.Name != client_.ObjectMeta.Name {
+				continue
+			}
+			// remove server options
+			service.Spec.BindAddr = ""
+
+			client_.Spec.Services[service.ObjectMeta.Name] = &service.Spec
+		}
+	}
+
+	// NOTE: Client does not need default service
+	//if len(client_.Spec.Services) == 0 {
+	//	// Create dummy service
+	//	serviceSpec := &ratholev1alpha1.RatholeServiceSpec{
+	//		ServerRef: ratholev1alpha1.RatholeServiceResourceRef{
+	//			Name: client_.ObjectMeta.Name,
+	//		},
+	//		Type:     "tcp",
+	//		Token:    "default",
+	//		BindAddr: "0.0.0.0:3030",
+	//	}
+	//	client_.Spec.Services["dummy"] = serviceSpec
+	//}
+
+	// Set default token if set .Spec.DefaultTokenFrom and .Spec.DefaultToken is empty
+	// TODO: If both .Spec.DefaultTokenFrom and .Spec.DefaultToken are set, an error should occur through the webhook validate
+	if client_.Spec.DefaultToken == "" {
+		if client_.Spec.DefaultTokenFrom.ConfigMapRef.Name != "" { // Use ConfigMap
+			var (
+				configMap corev1.ConfigMap
+				ok        bool
+			)
+			if err := r.Get(ctx, client.ObjectKey{Namespace: client_.Namespace, Name: client_.Spec.DefaultTokenFrom.ConfigMapRef.Name}, &configMap); err != nil {
+				return err
+			}
+			client_.Spec.DefaultToken, ok = configMap.Data[client_.Spec.DefaultTokenFrom.ConfigMapRef.Key]
+			if !ok {
+				return fmt.Errorf("key %s not found in configmap %s", client_.Spec.DefaultTokenFrom.ConfigMapRef.Key, client_.Spec.DefaultTokenFrom.ConfigMapRef.Name)
+			}
+		} else if client_.Spec.DefaultTokenFrom.SecretRef.Name != "" { // Use Secret
+			var (
+				secret        corev1.Secret
+				secretContent []byte
+				ok            bool
+			)
+			if err := r.Get(ctx, client.ObjectKey{Namespace: client_.Namespace, Name: client_.Spec.DefaultTokenFrom.SecretRef.Name}, &secret); err != nil {
+				return err
+			}
+			secretContent, ok = secret.Data[client_.Spec.DefaultTokenFrom.SecretRef.Key]
+			if !ok {
+				return fmt.Errorf("key %s not found in secret %s", client_.Spec.DefaultTokenFrom.SecretRef.Key, client_.Spec.DefaultTokenFrom.SecretRef.Name)
+			}
+			client_.Spec.DefaultToken = string(secretContent)
+		}
+	}
+
+	client_.Status.Condition.ObservedGeneration = client_.Generation
+	client_.Status.Condition.Status = "Synced"
+	client_.Status.Condition.Reason = "Reconciled"
+	// TODO: update last synced time
+	if err := r.Status().Update(ctx, client_); err != nil {
+		return err
+	}
+
+	tomlParent := "client"
+	config, err := ConvertSpecToToml(&tomlParent, client_.Spec)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Config: %s\n", config)
+
+	// upsert config to configTarget
+	configResourceType := strings.ToLower(client_.Spec.ConfigTarget.ResourceType)
+
+	ownerRefs := []metav1.OwnerReference{
+		{
+			APIVersion: client_.APIVersion,
+			Kind:       client_.Kind,
+			Name:       client_.Name,
+			UID:        client_.UID,
+		},
+	}
+
+	switch configResourceType {
+	case "secret":
+		var (
+			secret corev1.Secret
+			ok     = true
+		)
+
+		// search secret
+		if err := r.Get(ctx, client.ObjectKey{Namespace: client_.Namespace, Name: client_.Spec.ConfigTarget.Name}, &secret); err != nil {
+			if client.IgnoreNotFound(err) != nil {
+				return err
+			}
+			ok = false
+		}
+
+		// if not exist, create secret
+		if !ok {
+			secret = corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:            client_.Spec.ConfigTarget.Name,
+					Namespace:       client_.Namespace,
+					OwnerReferences: ownerRefs,
+				},
+				StringData: map[string]string{
+					"config.toml": config,
+				},
+			}
+			if err := r.Create(ctx, &secret); err != nil {
+				return err
+			}
+		} else {
+			secret.StringData["config.toml"] = config
+			if err := r.Update(ctx, &secret); err != nil {
+				return err
+			}
+		}
+
+	case "configmap":
+		var (
+			configMap corev1.ConfigMap
+
+			ok = true
+		)
+
+		// search configmap
+		if err := r.Get(ctx, client.ObjectKey{Namespace: client_.Namespace, Name: client_.Spec.ConfigTarget.Name}, &configMap); err != nil {
+			if client.IgnoreNotFound(err) != nil {
+				return err
+			}
+			ok = false
+		}
+
+		// if not exist, create configmap
+		if !ok {
+			configMap = corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:            client_.Spec.ConfigTarget.Name,
+					Namespace:       client_.Namespace,
 					OwnerReferences: ownerRefs,
 				},
 				Data: map[string]string{
